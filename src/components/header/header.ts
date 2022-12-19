17cb17c11e7a8b8548642b926b3080f8bcd84506
PHOENIXIT\ekanarskaya <ekanarskaya@fil-it.ru>
2022-12-19 15:38:51 +0300

    feat: add media-queries and view class for header

--- src/components/header/header.ts
+++ src/components/header/header.ts
@@ -1,53 +1,51 @@
+/* eslint-disable no-useless-call */
+import Render from '../abstracts/render'
 import './header'
-export default class Header {
-  renderHeaderContent (): void {
-    const fragment = document.createDocumentFragment()
-    const header: HTMLDivElement | null = document.querySelector('#header')
-    if (header instanceof HTMLTemplateElement) {
-      const headerClone = header.content.cloneNode(true)
-      fragment.append(headerClone)
-    }
+export default class Header extends Render {
+  private readonly CLASS_HEADER = 'header'
+  private readonly CLASS_HEADER_NAV: string = 'header__nav'
+  private readonly CLASS_CLOSED = 'closed'
+  private readonly CLASS_BURGER_ICON = 'burger__icon'
+  private readonly CLASS_HEADER_CLOSE_NAV = 'header__close-nav'
+  private readonly CLASS_SEARCH_BTN = 'search-button'
+  private readonly CLASS_SEARCH_INPUT = 'search__input'
+  private readonly CLASS_SEARCH_INPUT_VISIBLE = 'search__input_visible'
 
-    const headerComponent: HTMLDivElement | null = document.querySelector('.header')
-    headerComponent?.append(fragment)
+  renderHeaderContent (): void {
+    super.renderContent(this.CLASS_HEADER, this.CLASS_HEADER)
   }
 
   renderNavigation (): void {
-    const fragment = document.createDocumentFragment()
-    const nav: HTMLDivElement | null = document.querySelector('#header__nav')
-    if (nav instanceof HTMLTemplateElement) {
-      const navClone = nav.content.cloneNode(true)
-      fragment.append(navClone)
-    }
-
-    const headerNavigationComponent: HTMLElement | null = document.querySelector('.header__nav')
-    headerNavigationComponent?.append(fragment)
+    super.renderContent(this.CLASS_HEADER_NAV, this.CLASS_HEADER_NAV)
   }
 
   handleNavOpen (): void {
-    const navList: HTMLDivElement | null = document.querySelector('.header__nav')
-    if (navList != null) {
-      if (navList.classList.contains('closed')) {
-        navList.classList.remove('closed')
-      } else {
-        navList.classList.add('closed')
-      }
-    }
+    super.handleElementVisibility(this.CLASS_HEADER_NAV, this.CLASS_CLOSED)
+  }
+
+  handleSearchInput (): void {
+    super.handleElementVisibility(this.CLASS_SEARCH_INPUT, this.CLASS_SEARCH_INPUT_VISIBLE)
   }
 
   showNav (): void {
-    const burgerIcon = document.querySelector('.burger__icon')
-    burgerIcon?.addEventListener('click', this.handleNavOpen)
+    const burgerIcon = document.querySelector('.' + this.CLASS_BURGER_ICON)
+    burgerIcon?.addEventListener('click', () => this.handleNavOpen.call(this))
   }
 
   closeNav (): void {
-    const closeNavIcon = document.querySelector('.header__close-nav')
-    closeNavIcon?.addEventListener('click', this.handleNavOpen)
+    const closeNavIcon = document.querySelector('.' + this.CLASS_HEADER_CLOSE_NAV)
+    closeNavIcon?.addEventListener('click', () => this.handleNavOpen.call(this))
+  }
+
+  showSearch (): void {
+    const searchBtn = document.querySelector('.' + this.CLASS_SEARCH_BTN)
+    searchBtn?.addEventListener('click', () => this.handleSearchInput.call(this))
   }
 
   handleNav (): void {
     this.showNav()
     this.closeNav()
+    this.showSearch()
   }
 
   renderHeader (): void {
