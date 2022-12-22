4ce63a2e1cd0acdabff63323777deabd8eed1064
denismiron <denisbvb0@gmail.com>
2022-12-22 20:48:18 +0300

    feat: footer templating

--- src/components/app/app.ts
+++ src/components/app/app.ts
@@ -1,13 +1,16 @@
 import Header from '../header/header'
+import Footer from '../footer/footer'
 
 export default class App {
   private readonly view
-
+  private readonly footer
   constructor () {
     this.view = new Header()
+    this.footer = new Footer()
   }
 
   start (): void {
     this.view.renderHeader()
+    this.footer.renderFooter()
   }
 }
