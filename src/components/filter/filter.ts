419ec00cc82bfbbba9737879a551f34234eca14c
denismiron <denisbvb0@gmail.com>
2022-12-27 22:18:03 +0300

    feat: filters development

--- src/components/filter/filter.ts
+++ src/components/filter/filter.ts
@@ -1,4 +1,30 @@
-// import FilterObject from '../../utils/types/IFilterObject'
-// export default class Filters {
+/* eslint-disable */
+import { AppEvents } from '../../utils/enums/app-events'
 
-// }
+export default class FilterAccumulator {
+  private _filterSearch = '';
+  private _filterPrice = 1000;
+  constructor() {
+    document.addEventListener(AppEvents.EVENT_SEARCH, (event) => this.changeSymbolFilterHandler.call(this, <CustomEvent>event))
+    document.addEventListener(AppEvents.EVENT_PRICE, (event) => this.changeNumberFilterHandler.call(this, <CustomEvent>event))
+  }
+
+  private generateEvent() {
+    document.dispatchEvent(new CustomEvent(AppEvents.EVENT_FILTER, {
+      detail: {
+        text: this._filterSearch,
+        balance: this._filterPrice
+      }
+    }))
+  }
+
+  private changeSymbolFilterHandler(event: CustomEvent) {
+    this._filterSearch = event.detail?.text
+    this.generateEvent()
+  }
+
+  private changeNumberFilterHandler(event: CustomEvent) {
+    this._filterPrice = event.detail?.balance
+    this.generateEvent()
+  }
+}
